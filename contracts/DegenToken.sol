f5bf05082e24fc7fd2cb35cc5f2306a8c1f969e3
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-20 17:33:34 +0100

    Remove ownable since no functions need it.

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,23 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 /**
  * @notice The Degen token allows Farcaster community members to earn reputation tokens.
  */
-contract DegenToken is ERC20Burnable, ERC20Permit, Ownable {
+contract DegenToken is ERC20Burnable, ERC20Permit {
     string internal constant TOKEN_NAME = "Degen";
     string internal constant TOKEN_SYMBOL = "DEGEN";
     uint256 internal constant TOKEN_SUPPLY = 1_000_000_000;
 
-    constructor()
-        ERC20(TOKEN_NAME, TOKEN_SYMBOL)
-        ERC20Permit(TOKEN_NAME)
-        Ownable(msg.sender)
-    {
+    constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) ERC20Permit(TOKEN_NAME) {
         _mint(msg.sender, TOKEN_SUPPLY * 10 ** decimals());
     }
 }
