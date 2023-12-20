56c409836d5c4cec0a073d3960bfe98ab55577bc
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-20 17:43:43 +0100

    Incorporate elements from OpenZepplin wizard.

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.23;
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 /**
  * @notice The Degen token allows Farcaster community members to earn reputation tokens.
+ * @custom:security-contact jacek@degen.tips
  */
-contract DegenToken is ERC20Burnable, ERC20Permit {
+contract DegenToken is ERC20, ERC20Burnable, ERC20Permit {
     string internal constant TOKEN_NAME = "Degen";
     string internal constant TOKEN_SYMBOL = "DEGEN";
     uint256 internal constant TOKEN_SUPPLY = 1_000_000_000;
