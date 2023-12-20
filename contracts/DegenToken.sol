cf712b8dbffff3665887ee8ec6fe77926d4f5f2e
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-20 14:58:36 +0100

    Remove cap since there is no minting function.

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -3,13 +3,12 @@ pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 /**
  * @notice The  Degen token allows Farcaster community members to earn reputation tokens.
  */
-contract DegenToken is ERC20Burnable, ERC20Capped, ERC20Permit, Ownable {
+contract DegenToken is ERC20Burnable, ERC20Permit, Ownable {
     string internal constant TOKEN_NAME = "Degen";
     string internal constant TOKEN_SYMBOL = "DEGEN";
     uint256 internal constant TOKEN_SUPPLY = 1_000_000_000;
@@ -17,18 +16,8 @@ contract DegenToken is ERC20Burnable, ERC20Capped, ERC20Permit, Ownable {
     constructor()
         ERC20(TOKEN_NAME, TOKEN_SYMBOL)
         ERC20Permit(TOKEN_NAME)
-        ERC20Capped(TOKEN_SUPPLY * 10 ** decimals())
         Ownable(msg.sender)
     {
         _mint(msg.sender, TOKEN_SUPPLY * 10 ** decimals());
-    }
-
-    /// a function goes into a bar
-    function _update(
-        address from,
-        address to,
-        uint256 value
-    ) internal virtual override(ERC20, ERC20Capped) {
-        super._update(from, to, value);
     }
 }
