971e6553c4315ae66d59ace56bd48df91b711dac
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-20 14:22:34 +0100

    Add fixture to tests.

--- test/DegenToken.ts
+++ test/DegenToken.ts
@@ -1,11 +1,18 @@
+import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
 describe('Degen token contract', function () {
-  it('Deployment should assign the total supply of tokens to the owner', async function () {
-    const [owner] = await ethers.getSigners();
+  async function deployTokenFixture() {
+    const [owner, addr1, addr2] = await ethers.getSigners();
 
     const degenToken = await ethers.deployContract('DegenToken');
+
+    // Fixtures can return anything you consider useful for your tests
+    return { degenToken, owner, addr1, addr2 };
+  }
+  it('Should assign the total supply of tokens to the owner', async function () {
+    const { degenToken, owner } = await loadFixture(deployTokenFixture);
 
     const ownerBalance = await degenToken.balanceOf(owner.address);
     expect(await degenToken.totalSupply()).to.equal(ownerBalance);
