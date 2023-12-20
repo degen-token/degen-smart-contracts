88d547993ac882b7d76df6d538b0536ed3396689
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-20 13:16:38 +0100

    Install Docgen.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import 'solidity-docgen';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.23',
