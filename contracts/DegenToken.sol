2829f7970a1c6b94ac767cdb8b615bc29d811deb
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-20 21:35:54 +0100

    Remove permit functionality since we'll use a merkle tree.

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -3,18 +3,17 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 /**
  * @notice The Degen token allows Farcaster community members to earn reputation tokens.
  * @custom:security-contact jacek@degen.tips
  */
-contract DegenToken is ERC20, ERC20Burnable, ERC20Permit {
+contract DegenToken is ERC20, ERC20Burnable {
     string internal constant TOKEN_NAME = "Degen";
     string internal constant TOKEN_SYMBOL = "DEGEN";
     uint256 internal constant TOKEN_SUPPLY = 1_000_000_000;
 
-    constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) ERC20Permit(TOKEN_NAME) {
+    constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) {
         _mint(msg.sender, TOKEN_SUPPLY * 10 ** decimals());
     }
 }
