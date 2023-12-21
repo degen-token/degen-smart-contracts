8dc0424988216375275f2a308176d66a500d26b0
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-22 00:08:03 +0100

    Add merkle root as a parameter.

--- ignition/modules/DegenModule.ts
+++ ignition/modules/DegenModule.ts
@@ -1,12 +1,14 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
+const MERKLE_ROOT =
+  '0xdefa96435aec82d201dbd2e5f050fb4e1fef5edac90ce1e03953f916a5e1132d';
+
 export default buildModule('DegenModule', (m) => {
+  const merkleRoot = m.getParameter('merkleRoot', MERKLE_ROOT);
+
   const degenToken = m.contract('DegenToken');
 
-  const degenAirdrop = m.contract('DegenAirdrop', [
-    degenToken,
-    '0xdefa96435aec82d201dbd2e5f050fb4e1fef5edac90ce1e03953f916a5e1132d',
-  ]);
+  const degenAirdrop = m.contract('DegenAirdrop', [degenToken, merkleRoot]);
 
   return { degenToken, degenAirdrop };
 });
