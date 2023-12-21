dde7c75b6bc02bed9ef055adbb0d620491487e99
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-21 09:51:30 +0100

    Remove docgen config.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,5 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
-import 'solidity-docgen';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
