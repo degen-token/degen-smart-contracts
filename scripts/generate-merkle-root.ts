75304e1bea045063f73bff8f2e4fba781a2ed761
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-21 20:00:50 +0100

    Fix filename that results are written to for the merkel root.

--- scripts/generate-merkle-root.ts
+++ scripts/generate-merkle-root.ts
@@ -5,6 +5,7 @@
  */
 import { program } from 'commander';
 import fs from 'fs';
+import path from 'path';
 import { parseBalanceMap } from './parse-balance-map';
 
 var json;
@@ -18,13 +19,15 @@ program
 
 program.parse();
 
-json = JSON.parse(fs.readFileSync(program.opts().input, { encoding: 'utf8' }));
+const filePath = program.opts().input;
+json = JSON.parse(fs.readFileSync(filePath, { encoding: 'utf8' }));
 
 if (typeof json !== 'object') throw new Error('Invalid JSON');
 
 const result = JSON.stringify(parseBalanceMap(json));
-fs.writeFileSync('./airdrop/airdrop_merkle_root.json', result);
+const fileName = path.basename(filePath, path.extname(filePath));
+fs.writeFileSync(`./airdrop/${fileName}_merkle_root.json`, result);
 
 console.log(
-  'Merkle root generated successfully: ./airdrop/airdrop_merkle_root.json'
+  `Merkle root generated successfully: ./airdrop/${fileName}_merkle_root.json`
 );
