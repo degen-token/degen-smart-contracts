b7d9597bad0793dbceb010b6f0141d440d81bfe9
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-21 23:05:23 +0100

    Remove script that was renamed.

--- ignition/modules/DegenToken.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
-
-export default buildModule('DegenToken', (m) => {
-  const degenToken = m.contract('DegenToken');
-
-  return { degenToken };
-});
