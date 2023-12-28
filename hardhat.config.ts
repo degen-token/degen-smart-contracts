f8c54039bcf5222c4292e4fa396473b7a4b881bd
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-28 18:41:22 +0100

    Add Base networks to Hardhat config.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,33 @@
-import type { HardhatUserConfig } from 'hardhat/config';
-
 import '@nomicfoundation/hardhat-ignition-ethers';
 import '@nomicfoundation/hardhat-toolbox';
+import dotenv from 'dotenv';
+import type { HardhatUserConfig } from 'hardhat/config';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
+  networks: {
+    // for mainnet
+    'base-mainnet': {
+      url: 'https://mainnet.base.org',
+      accounts: [process.env.PRIVATE_KEY as string],
+      gasPrice: 1000000000,
+    },
+    // for testnet
+    'base-goerli': {
+      url: 'https://goerli.base.org',
+      accounts: [process.env.PRIVATE_KEY as string],
+      gasPrice: 1000000000,
+    },
+    // for local dev environment
+    'base-local': {
+      url: 'http://localhost:8545',
+      accounts: [process.env.PRIVATE_KEY as string],
+      gasPrice: 1000000000,
+    },
+  },
+  defaultNetwork: 'base-local',
 };
 
 export default config;
