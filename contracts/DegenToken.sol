3036174143a9f88eb17c29e296f302ba75fa9392
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-29 00:05:44 +0100

    Change the token supply to 7 billion.

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -11,7 +11,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 contract DegenToken is ERC20, ERC20Burnable {
     string internal constant TOKEN_NAME = "Degen";
     string internal constant TOKEN_SYMBOL = "DEGEN";
-    uint256 internal constant TOKEN_SUPPLY = 1_000_000_000;
+    uint256 internal constant TOKEN_SUPPLY = 7_000_000_000;
 
     constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) {
         _mint(msg.sender, TOKEN_SUPPLY * 10 ** decimals());
