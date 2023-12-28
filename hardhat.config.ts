d605bb55700f8f46a0dec080805b6d008e61cd92
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-29 00:40:30 +0100

    Remove gas from config.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,19 +12,16 @@ const config: HardhatUserConfig = {
     'base-mainnet': {
       url: 'https://mainnet.base.org',
       accounts: [process.env.PRIVATE_KEY as string],
-      gasPrice: 1000000000,
     },
     // for testnet
     'base-goerli': {
       url: 'https://goerli.base.org',
       accounts: [process.env.PRIVATE_KEY as string],
-      gasPrice: 1000000000,
     },
     // for local dev environment
     'base-local': {
       url: 'http://localhost:8545',
       accounts: [process.env.PRIVATE_KEY as string],
-      gasPrice: 1000000000,
     },
   },
 };
