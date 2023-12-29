18679c5c09068d7e60b600f917ce71a15aa7080c
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-29 01:17:58 +0100

    Add contract verification scripts.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import '@nomicfoundation/hardhat-ignition-ethers';
 import '@nomicfoundation/hardhat-toolbox';
+import '@nomicfoundation/hardhat-verify';
 import dotenv from 'dotenv';
 import type { HardhatUserConfig } from 'hardhat/config';
 
@@ -9,12 +10,12 @@ const config: HardhatUserConfig = {
   solidity: '0.8.20',
   networks: {
     // for mainnet
-    'base-mainnet': {
+    base_mainnet: {
       url: 'https://mainnet.base.org',
       accounts: [process.env.PRIVATE_KEY as string],
     },
     // for testnet
-    'base-goerli': {
+    base_goerli: {
       url: 'https://goerli.base.org',
       accounts: [process.env.PRIVATE_KEY as string],
     },
@@ -22,6 +23,11 @@ const config: HardhatUserConfig = {
     'base-local': {
       url: 'http://localhost:8545',
       accounts: [process.env.PRIVATE_KEY as string],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      baseGoerli: process.env.BASESCAN_API_KEY ?? '',
     },
   },
 };
