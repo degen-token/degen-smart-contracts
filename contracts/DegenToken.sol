8fb4b996f34884d7eca567c7bddbe2344d705980
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-30 16:55:59 +0100

    Make the contract Ownable.

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -3,17 +3,18 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @notice The Degen token allows Farcaster community members to earn reputation tokens.
  * @custom:security-contact jacek@degen.tips
  */
-contract DegenToken is ERC20, ERC20Burnable {
+contract DegenToken is ERC20, ERC20Burnable, Ownable {
     string internal constant TOKEN_NAME = "Degen";
     string internal constant TOKEN_SYMBOL = "DEGEN";
     uint256 internal constant TOKEN_SUPPLY = 7_000_000_000;
 
-    constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) {
+    constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) Ownable(msg.sender) {
         _mint(msg.sender, TOKEN_SUPPLY * 10 ** decimals());
     }
 }
