82c78cd27df961b67fd1e0b5dc60f0796f30810c
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-31 18:28:17 +0100

    Add a minting data parameter to the deploy module.

--- ignition/modules/DegenModule.ts
+++ ignition/modules/DegenModule.ts
@@ -3,11 +3,11 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 const MERKLE_ROOT =
   '0xb1cf927d4e1cbac2bf6de588d7604a88b3cc8c3db561b5cd568ccaf7c6d5c9fa';
 
+const NEXT_MINTING_DATE = Math.round(new Date('2025-01-01').getTime() / 1000);
+
 export default buildModule('DegenModule', (m) => {
   const merkleRoot = m.getParameter('merkleRoot', MERKLE_ROOT);
-
-  // Get the time for January 1, 2025 in seconds since the Unix Epoch
-  const nextMintingDate = Math.round(new Date('2025-01-01').getTime() / 1000);
+  const nextMintingDate = m.getParameter('merkleRoot', NEXT_MINTING_DATE);
 
   const degenToken = m.contract('DegenToken', [nextMintingDate]);
 
