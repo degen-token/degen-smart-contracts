b53051440402116de2fb913318e2a760b92416fe
Jacek Trocinski <jacek@jtrocinski.com>
2023-12-31 17:06:11 +0100

    Add 1% inflation to the token by adding a minting function.

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -6,15 +6,82 @@ import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC2
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- * @notice The Degen token allows Farcaster community members to earn reputation tokens.
+ * @notice The Degen token
  * @custom:security-contact jacek@degen.tips
  */
 contract DegenToken is ERC20, ERC20Burnable, Ownable {
-    string internal constant TOKEN_NAME = "Degen";
-    string internal constant TOKEN_SYMBOL = "DEGEN";
-    uint256 internal constant TOKEN_SUPPLY = 7_000_000_000;
+    /**
+     * @dev EIP-20 token name for this token
+     */
+    string public constant TOKEN_NAME = "Degen";
 
-    constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) Ownable(msg.sender) {
+    /**
+     * @dev EIP-20 token symbol for this token
+     */
+    string public constant TOKEN_SYMBOL = "DEGEN";
+
+    /**
+     * @dev Total number of tokens in circulation
+     */
+    uint256 public constant TOKEN_SUPPLY = 7_000_000_000;
+
+    /**
+     * @dev The timestamp after which minting may occur
+     */
+    uint256 public mintingAllowedAfter;
+
+    /**
+     * @dev Minimum time between mints
+     */
+    uint32 public constant minimumTimeBetweenMints = 1 days * 365;
+
+    /**
+     * @dev Cap on the percentage of totalSupply that can be minted at each mint
+     */
+    uint8 public constant mintCap = 1;
+
+    /**
+     * @dev Construct a new Degen token
+     * @param mintingAllowedAfter_ The timestamp after which minting may occur
+     */
+    constructor(
+        uint256 mintingAllowedAfter_
+    ) ERC20(TOKEN_NAME, TOKEN_SYMBOL) Ownable(msg.sender) {
+        require(
+            mintingAllowedAfter_ >= block.timestamp,
+            "Degen::constructor: minting can only begin after deployment"
+        );
+
         _mint(msg.sender, TOKEN_SUPPLY * 10 ** decimals());
+
+        mintingAllowedAfter = mintingAllowedAfter_;
+    }
+
+    /**
+     * @dev Mint new tokens
+     * @param to The address of the target account
+     * @param amount The number of tokens to be minted
+     */
+    function mint(address to, uint96 amount) public onlyOwner {
+        require(
+            block.timestamp >= mintingAllowedAfter,
+            "Degen::mint: minting not allowed yet"
+        );
+
+        require(
+            to != address(0),
+            "Degen::mint: cannot transfer to the zero address"
+        );
+
+        // record the mint
+        mintingAllowedAfter = block.timestamp + minimumTimeBetweenMints;
+
+        // mint the amount
+        require(
+            amount <= (totalSupply() * mintCap) / 100,
+            "Degen::mint: exceeded mint cap"
+        );
+
+        _mint(to, amount);
     }
 }
