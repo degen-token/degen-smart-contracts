fc0e062ee344b5a935623182759d3eb8c955a724
Jacek Trocinski <jacek@jtrocinski.com>
2024-01-02 18:27:44 +0100

    Fix: Pragma directives should be fixed to clearly identify the Solidity version with which the contracts will be compiled.

--- contracts/DegenAirdrop.sol
+++ contracts/DegenAirdrop.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
