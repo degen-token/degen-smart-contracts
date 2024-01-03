5b06f38625258bb810dc8db6b632a8f2919eb4c2
Jacek Trocinski <jacek@jtrocinski.com>
2024-01-03 15:58:26 +0100

    Add airdrop claim deadline.

--- ignition/modules/DegenModule.ts
+++ ignition/modules/DegenModule.ts
@@ -5,13 +5,25 @@ const MERKLE_ROOT =
 
 const NEXT_MINTING_DATE = Math.round(new Date('2025-01-01').getTime() / 1000);
 
+const AIRDROP_CLAIM_DEADLINE = Math.round(
+  new Date('2024-06-01').getTime() / 1000
+);
+
 export default buildModule('DegenModule', (m) => {
   const merkleRoot = m.getParameter('merkleRoot', MERKLE_ROOT);
   const nextMintingDate = m.getParameter('nextMintingDate', NEXT_MINTING_DATE);
+  const airdropClaimDeadline = m.getParameter(
+    'airdropClaimDeadline',
+    AIRDROP_CLAIM_DEADLINE
+  );
 
   const degenToken = m.contract('DegenToken', [nextMintingDate]);
 
-  const degenAirdrop = m.contract('DegenAirdrop', [degenToken, merkleRoot]);
+  const degenAirdrop = m.contract('DegenAirdrop', [
+    degenToken,
+    merkleRoot,
+    airdropClaimDeadline,
+  ]);
 
   return { degenToken, degenAirdrop };
 });
