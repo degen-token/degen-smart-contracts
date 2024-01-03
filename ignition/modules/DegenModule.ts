bbbe14be2cca9db0de18701aba17ed480678536e
Jacek Trocinski <jacek@jtrocinski.com>
2024-01-03 14:06:53 +0100

    Change merkle root.

--- ignition/modules/DegenModule.ts
+++ ignition/modules/DegenModule.ts
@@ -1,7 +1,7 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
 const MERKLE_ROOT =
-  '0xb1cf927d4e1cbac2bf6de588d7604a88b3cc8c3db561b5cd568ccaf7c6d5c9fa';
+  '0x3edf309d20f61e89fa130972112cc09d88b7c2cbd599f4f8e3c7b4e06c517e63';
 
 const NEXT_MINTING_DATE = Math.round(new Date('2025-01-01').getTime() / 1000);
 
