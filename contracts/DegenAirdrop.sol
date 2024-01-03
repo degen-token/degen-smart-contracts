42425aafec81001337cecffe8cca92237e0783c3
Jacek Trocinski <jacek@jtrocinski.com>
2024-01-03 14:45:10 +0100

    Change SPDX license.

--- contracts/DegenAirdrop.sol
+++ contracts/DegenAirdrop.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.20;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
