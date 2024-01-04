8e75b72ead6528ee4422f7813fd0de4dec8f46f8
Jacek Trocinski <jacek@jtrocinski.com>
2024-01-04 14:40:06 +0100

    Add comments to Ignition deploy module.

--- ignition/modules/DegenModule.ts
+++ ignition/modules/DegenModule.ts
@@ -1,5 +1,8 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
+/**
+ * Airdrop 1 Constants
+ */
 const AIRDROP1_CLAIM_DEADLINE = Math.round(
   new Date('2024-05-01').getTime() / 1000
 );
@@ -9,11 +12,17 @@ const AIRDROP1_MERKLE_ROOT =
 
 const AIRDROP1_TRANSFER_AMOUNT = 1000000000n * 10n ** 18n;
 
+/**
+ * Degen Token Constants
+ */
 const TOKEN_NEXT_MINTING_DATE = Math.round(
   new Date('2028-01-01').getTime() / 1000
 );
 
 export default buildModule('DegenModule', (m) => {
+  /**
+   * Parameters
+   */
   const airdrop1MerkleRoot = m.getParameter(
     'airdrop1MerkleRoot',
     AIRDROP1_MERKLE_ROOT
@@ -27,14 +36,19 @@ export default buildModule('DegenModule', (m) => {
     AIRDROP1_CLAIM_DEADLINE
   );
 
+  /**
+   * Contracts
+   */
   const degenToken = m.contract('DegenToken', [nextMintingDate]);
-
   const degenAirdrop1 = m.contract('DegenAirdrop1', [
     degenToken,
     airdrop1MerkleRoot,
     airdrop1ClaimDeadline,
   ]);
 
+  /**
+   * Transactions
+   */
   m.call(degenToken, 'transfer', [degenAirdrop1, AIRDROP1_TRANSFER_AMOUNT]);
 
   return { degenToken, degenAirdrop1 };
