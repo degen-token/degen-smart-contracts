dc752fb567369f0ce11fa66f7585728642c28417
Jacek Trocinski <jacek@jtrocinski.com>
2024-01-05 11:07:36 +0100

    Update import statements.

--- contracts/DegenAirdrop1.sol
+++ contracts/DegenAirdrop1.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.20;
 
-import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @notice Distributes a balance of tokens according to a merkle root.
