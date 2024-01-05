25ca786c39751b779b962c2e1dac9addc5f86435
Jacek Trocinski <jacek@jtrocinski.com>
2024-01-05 11:30:13 +0100

    Add explicit imports back.

--- contracts/DegenAirdrop1.sol
+++ contracts/DegenAirdrop1.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
