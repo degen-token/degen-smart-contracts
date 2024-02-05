6e1177b0aba9ce28675fdd54e31cc3fceb8b097f
Jacek Trocinski <jacek@jtrocinski.com>
2024-02-05 12:36:08 +0100

    Add vesting testcases.

--- contracts/OtcVesting.sol
+++ contracts/OtcVesting.sol
@@ -2,8 +2,33 @@
 pragma solidity 0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract OtcVesting {
+contract OtcVesting is Ownable {
+    /* ====== Errors ======== */
+
+    /**
+     *  @dev The vesting start date is in the past
+     */
+    error VestingStartDateInPast();
+
+    /**
+     *  @dev The vesting cliff date is in the past
+     */
+    error VestingCliffDateInPast();
+
+    /**
+     *  @dev The vesting end date is before the cliff date
+     */
+    error VestingEndDateBeforeCliffDate();
+
+    /**
+     *  @dev The vesting cliff date has not been reached yet
+     */
+    error VestingCliffDateNotReached();
+
+    /* ======== State Variables ======= */
+
     address public immutable DEGEN;
     address public recipient;
 
@@ -21,19 +46,11 @@ contract OtcVesting {
         uint256 vestingBegin_,
         uint256 vestingCliff_,
         uint256 vestingEnd_
-    ) {
-        require(
-            vestingBegin_ >= block.timestamp,
-            "TreasuryVester.constructor: vesting begin too early"
-        );
-        require(
-            vestingCliff_ >= vestingBegin_,
-            "TreasuryVester.constructor: cliff is too early"
-        );
-        require(
-            vestingEnd_ > vestingCliff_,
-            "TreasuryVester.constructor: end is too early"
-        );
+    ) Ownable(recipient_) {
+        if (vestingBegin_ < block.timestamp) revert VestingStartDateInPast();
+        if (vestingCliff_ < block.timestamp) revert VestingCliffDateInPast();
+        if (vestingEnd_ <= vestingCliff_)
+            revert VestingEndDateBeforeCliffDate();
 
         DEGEN = degen_;
         recipient = recipient_;
@@ -46,19 +63,10 @@ contract OtcVesting {
         lastUpdate = VESTING_BEGIN;
     }
 
-    function setRecipient(address recipient_) public {
-        require(
-            msg.sender == recipient,
-            "TreasuryVester.setRecipient: unauthorized"
-        );
-        recipient = recipient_;
-    }
+    function claim() external onlyOwner {
+        if (block.timestamp < VESTING_CLIFF)
+            revert VestingCliffDateNotReached();
 
-    function claim() public {
-        require(
-            block.timestamp >= VESTING_CLIFF,
-            "TreasuryVester.claim: not time yet"
-        );
         uint256 amount;
         if (block.timestamp >= VESTING_END) {
             amount = IERC20(DEGEN).balanceOf(address(this));
@@ -71,7 +79,7 @@ contract OtcVesting {
         IERC20(DEGEN).transfer(recipient, amount);
     }
 
-    function recoverToken(address token_) public {
+    function recoverToken(address token_) external onlyOwner {
         require(
             token_ != DEGEN,
             "TreasuryVester.recoverToken: only recover tokens accidentally sent to the contract"
