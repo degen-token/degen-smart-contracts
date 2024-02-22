cbec6292f405f1d967da6c40fb0d5c472336add4
Jacek Trocinski <jacek@jtrocinski.com>
2024-02-22 11:27:13 +0100

    Update airdrop deployment for production.

--- ignition/modules/LiquidityMiningAirdrop1.ts
+++ ignition/modules/LiquidityMiningAirdrop1.ts
@@ -3,10 +3,10 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 /**
  * Airdrop Constants
  */
-const DEGEN_TOKEN = '0xfee293840D23B0B2De8C55e1Cf7A9F01C157767c';
+const DEGEN_TOKEN = '0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed';
 
 const AIRDROP_CLAIM_DEADLINE = Math.round(
-  new Date('2024-04-07').getTime() / 1000
+  new Date('2024-04-11').getTime() / 1000
 );
 
 const AIRDROP_MERKLE_ROOT =
