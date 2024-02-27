23b7543dcb8df0e455900fff89e119e807c596e7
Jacek Trocinski <jacek@jtrocinski.com>
2024-02-27 15:42:22 +0100

    Add Airdrop2Season1 Ignition module.

---
+++ ignition/modules/Airdrop2Season1.ts
@@ -0,0 +1,44 @@
+import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
+
+/**
+ * Airdrop Constants
+ */
+const DEGEN_TOKEN = '0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed';
+
+const AIRDROP_CLAIM_DEADLINE = Math.round(
+  new Date('2024-03-31').getTime() / 1000
+);
+
+const AIRDROP_MERKLE_ROOT =
+  '0x64e42863e6ad6dd9787aef425f98d76167177da3a0c9078448542b831d51efc2';
+
+const Airdrop2Season1 = buildModule('Airdrop2Season1', (m) => {
+  /**
+   * Parameters
+   */
+  const airdropMerkleRoot = m.getParameter(
+    'airdropMerkleRoot',
+    AIRDROP_MERKLE_ROOT
+  );
+  const airdropClaimDeadline = m.getParameter(
+    'airdropClaimDeadline',
+    AIRDROP_CLAIM_DEADLINE
+  );
+
+  /**
+   * Contracts
+   */
+  const degenAirdrop = m.contract('DegenAirdrop1', [
+    DEGEN_TOKEN,
+    airdropMerkleRoot,
+    airdropClaimDeadline,
+  ]);
+
+  /**
+   * Transactions
+   */
+
+  return { degenAirdrop };
+});
+
+export default Airdrop2Season1;
