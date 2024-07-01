82e4e50d0abadfbdfe3433ae66e45536894483b8
Jacek <jacek@degen.tips>
2024-07-01 13:11:53 +0200

    Update deadline

--- ignition/modules/DegenLock.ts
+++ ignition/modules/DegenLock.ts
@@ -4,7 +4,7 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
  * Lock Constants
  */
 const DEGEN_TOKEN = '0xfee293840D23B0B2De8C55e1Cf7A9F01C157767c';
-const DEPOSIT_DEADLINE = Math.round(new Date('2024-07-01').getTime() / 1000);
+const DEPOSIT_DEADLINE = Math.round(new Date('2024-07-02').getTime() / 1000);
 const LOCK_DURATION = 60 * 60; // 1 hour
 
 const DegenLock = buildModule('DegenLock', (m: any) => {
