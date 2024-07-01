f4331ae6e841dfafaa6ab16db2c0aa4e89e3516b
Jacek <jacek@degen.tips>
2024-07-01 12:02:04 +0200

    Keep standard names

--- contracts/DegenLock.sol
+++ contracts/DegenLock.sol
@@ -13,12 +13,12 @@ contract DegenLock is Ownable, IERC20 {
     /**
      * @dev Name of the token representing the claim on the locked token
      */
-    string public constant NAME = "Locked Degen";
+    string public constant name = "Locked Degen";
 
     /**
      * @dev Symbol of the token representing the claim on the locked token
      */
-    string public constant SYMBOL = "LDEGEN";
+    string public constant symbol = "LDEGEN";
 
     /**
      * @dev The ERC20 token to be locked
