9efc69de74d0be3bb617587aa01cb78dcca327d0
Jacek <jacek@degen.tips>
2024-07-04 16:49:57 +0200

    Fix solidity version

--- contracts/DegenLockToken.sol
+++ contracts/DegenLockToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity 0.8.20;
+pragma solidity ^0.8.0;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
