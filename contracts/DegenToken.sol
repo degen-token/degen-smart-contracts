abfc2a444a723dbbd31bbbc8e8cac7b734010e8f
Ruhul <ruhul20@gmail.com>
2024-07-05 00:31:36 +0600

    optimizing and updating code
    changing lock duration and min deposit to private and use getter method to get data
     emit
    removing DepositTimestampUpdated(msg.sender, block.timestamp); to save gas
    
    Wrong function signature for nonces:
    he nonces function should be overridden with ERC20Permit only.

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -144,9 +144,10 @@ contract DegenToken is
         super._update(from, to, value);
     }
 
+    // he nonces function should be overridden with ERC20Permit only.
     function nonces(
         address owner
-    ) public view override(ERC20Permit, Nonces) returns (uint256) {
+    ) public view override(ERC20Permit) returns (uint256) {
         return super.nonces(owner);
     }
 }
