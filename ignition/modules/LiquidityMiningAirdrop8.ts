0b4271f97cc76f16f2aca14ffd4340039e13662e
Jacek <jacek@degen.tips>
2025-03-31 13:03:54 +0200

    Add LiquidityMiningAirdrop8 smart contract.

---
+++ ignition/modules/LiquidityMiningAirdrop8.ts
@@ -0,0 +1,44 @@
+import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
+
+/**
+ * Airdrop Constants
+ */
+const DEGEN_TOKEN = '0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed';
+
+const AIRDROP_CLAIM_DEADLINE = Math.round(
+  new Date('2025-04-25').getTime() / 1000
+);
+
+const AIRDROP_MERKLE_ROOT =
+  '0x3d2943a60e83e71b940c8d4a2b1fe8829971c08d4d915a6fea24ec91ffd46c09';
+
+const LiquidityMiningAirdrop8 = buildModule('LiquidityMiningAirdrop8', (m) => {
+  /**
+   * Parameters
+   */
+  const airdropMerkleRoot = m.getParameter(
+    'airdropMerkleRoot',
+    AIRDROP_MERKLE_ROOT
+  );
+  const airdropClaimDeadline = m.getParameter(
+    'airdropClaimDeadline',
+    AIRDROP_CLAIM_DEADLINE
+  );
+
+  /**
+   * Contracts
+   */
+  const degenAirdrop = m.contract('DegenAirdrop1', [
+    DEGEN_TOKEN,
+    airdropMerkleRoot,
+    airdropClaimDeadline,
+  ]);
+
+  /**
+   * Transactions
+   */
+
+  return { degenAirdrop };
+});
+
+export default LiquidityMiningAirdrop8;
