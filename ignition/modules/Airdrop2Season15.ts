4e4977efdd10a59c89f0d60f84e7578f89e69910
Jacek <jacek@degen.tips>
2025-04-25 11:22:54 +0200

    Add season 15 smart contract.

---
+++ ignition/modules/Airdrop2Season15.ts
@@ -0,0 +1,44 @@
+import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
+
+/**
+ * Airdrop Constants
+ */
+const DEGEN_TOKEN = '0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed';
+
+const AIRDROP_CLAIM_DEADLINE = Math.round(
+  new Date('2025-05-30').getTime() / 1000
+);
+
+const AIRDROP_MERKLE_ROOT =
+  '0xbd134a60439798485b8d4555a340a7db08f176bbf6e8c4faacff0891ba6aceb6';
+
+const Airdrop2Season15 = buildModule('Airdrop2Season15', (m) => {
+  /**
+   * Parameters
+   */
+  const airdropMerkleRoot = m.getParameter(
+    'airdropMerkleRoot',
+    AIRDROP_MERKLE_ROOT
+  );
+  const airdropClaimDeadline = m.getParameter(
+    'airdropClaimDeadline',
+    AIRDROP_CLAIM_DEADLINE
+  );
+
+  /**
+   * Contracts
+   */
+  const degenAirdrop = m.contract('DegenAirdrop1', [
+    DEGEN_TOKEN,
+    airdropMerkleRoot,
+    airdropClaimDeadline,
+  ]);
+
+  /**
+   * Transactions
+   */
+
+  return { degenAirdrop };
+});
+
+export default Airdrop2Season15;
